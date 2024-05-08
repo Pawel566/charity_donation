7cc5b8192cd32433ecee1a669ff032f94b112d39
Pawel566 <pawel.garbaty@onet.pl>
2024-05-08 14:33:07 +0200

    Start project

---
+++ Charity_donation/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'charity_donation',
+        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'USER': 'postgres',
+        'PORT': 5432
+    }
+}
